f028b18adbf79fcf7bdeefa869520954ff05f4ad
jiayangchen <chenjiayang1207@163.com>
2016-08-27 21:08:23 +0800

    20160827

--- app/src/main/java/me/chenjiayang/myleancloud/EditInfoActivity.java
+++ app/src/main/java/me/chenjiayang/myleancloud/EditInfoActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
